d4ceeaa4c2b9ea1d8f593da25ba6910bf0550ebc
artemmoskalev92 <83223168+artemmoskalev92@users.noreply.github.com>
2022-01-10 15:51:06 +0500

    2

---
+++ Doodle Jump/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+   
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (target.transform.position.y > transform.position.y)
+        {
+            Vector3 newPos = new Vector3(transform.position.x, target.position.y, transform.position.z);
+            transform.position = newPos;
+        }
+    }
+}
